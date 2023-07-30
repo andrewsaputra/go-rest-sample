9fe372c75da084fee1ada0cd21dcf481dd724f40
Andrew Saputra <andrew.saputra@gmail.com>
2023-07-30 16:42:39 +0700

    add healthcheck

--- main.go
+++ main.go
@@ -9,17 +9,22 @@ import (
 
 func main() {
 	router := gin.Default()
+	router.GET("/health", healthCheck)
 	router.GET("/albums", getAlbums)
 	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
-	router.Run("localhost:8080")
+	router.Run("0.0.0.0:8080")
 }
 
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func healthCheck(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"status": "OK"})
 }
 
 func getAlbums(c *gin.Context) {
